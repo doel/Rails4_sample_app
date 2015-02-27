37afb76b8978824150dd893ea77839e67dccdfe1
Doel Sengupta <doels11@gmail.com>
2015-02-27 05:22:01 +0000

    signup with create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
 
   def show
    @user=User.find(params[:id])
-end
+  end
 
+  def create
+    @user=User.new(user_params)
+     if @user.save
+     else
+     @user-User.new
+    end
+  end
+
+
+  private
+
+    def user_params
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
