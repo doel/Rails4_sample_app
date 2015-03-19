44c6584370b04575d6b4755d40295dfc4e3b0114
Doel Sengupta <doels11@gmail.com>
2015-03-19 04:33:33 +0000

    password reset

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
+require 'pry'
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
   
   test "invalid signup information" do
     get signup_path
@@ -12,16 +17,39 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-    post_via_redirect users_path, user: {name: "doel",
+    post users_path, user: {name: "doel",
                                          email: "doel@a.com",
                                          password: "123456",
-                                         passowrd_confirmation: "123456"}
+                                         passowrd_confirmation: "123456",
+                                         activated: false,
+                                        activated_at: Time.zone.now 
+    }
     end
-    #assert_template 'users/show'
-    #assert is_logged_in?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    binding.pry
+    assert_not user.activated?
+    #Try to login before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    
+    #Try login with invalid activation code
+    get edit_account_activation_path("inavlid token")
+    assert_not is_logged_in?
+    
+    #Try login with invalid email, valid token
+    get edit_account_activation_path(user.activation_token, email: "inavlid email")
+    assert_not is_logged_in?
+    
+    #Try login with ivalid crentials
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 
 end
